17b8dc92cdf70dbbe1bc69d1551a42497cb04948
thenayr <iamnayr@gmail.com>
2015-06-20 18:58:32 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var server = require('http').Server(express);
+var DeepstreamClient = require( 'deepstream.io-client-js' );
+
+//var io = require('socket.io');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/docker-start', function(req, res) {
+  console.log('something happened in docker');
+})
+
+
+module.exports = router;
